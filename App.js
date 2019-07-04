575a7b32a2e43c19dca9e4e3851a00ad586a1344
athan <athan.gouf@gmail.com>
2019-07-04 20:30:51 +0300

    GetCurrentLocation

--- App.js
+++ App.js
@@ -17,10 +17,35 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          latitude: null,
+          longitude: null,
+          error: null,
+      };
+  }
+
+  componentDidMount() {
+      navigator.geolocation.getCurrentPosition(
+          (position) => {
+              this.setState({
+                  latitude: position.coords.latitude,
+                  longitude: position.coords.longitude,
+                  error: null,
+              });
+          },
+          (error) => this.setState({ error: error.message }),
+          { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+      );
+  }
   render() {
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <Text>Details Screen</Text>
+          <Text>Latitude: {this.state.latitude}</Text>
+          <Text>Longitude: {this.state.longitude}</Text>
+          {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
         </View>
     );
   }
